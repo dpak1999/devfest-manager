aff6e803f81f4f30512f4fa01ea86740bf3bf96a
Deepak Kumar <deepakkumardash313@gmail.com>
2026-02-25 02:00:14 +0530

    feat: Add `title`, `image`, and `date` inputs to `EventCard` and populate them in `EventList`.

--- src/app/features/events/event-list.ts
+++ src/app/features/events/event-list.ts
@@ -15,9 +15,13 @@ import { EventCard } from './event-card';
       <!-- TODO Mod 2: Use @for to iterate over resource -->
 
       <!-- Static Placeholders for initial verify -->
-      <app-event-card />
-      <app-event-card />
-      <app-event-card />
+      <app-event-card
+        title="Angular key note"
+        image="images/angular-keynote.png"
+        date="2025-12-10T09:00:00.000Z"
+      />
+      <app-event-card title="Signal deep dive" image="images/signals-deep-dive.png" />
+      <!-- <app-event-card /> -->
     </div>
   `,
 })
